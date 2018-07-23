fb20f2d400639b049602b16989041c1d09b04695
warukira <dwarukira@gmail.com>
2018-07-23 17:07:16 +0300

    [ch #159054005] Upgrade django from version 1.1 to 2.0

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,10 +1,14 @@
-# import jwt
-#
-# from django.conf import settings
-#
-# from rest_framework import authentication, exceptions
-#
-# from .models import User
+import jwt
+
+from django.conf import settings
+
+from rest_framework import authentication, exceptions
+
+from .models import User
 
 """Configure JWT Here"""
+class JWTAuthentication:
+    ''' JWTAuthentication imprements jwt authentication '''
 
+    def authenticate(self, request):
+        pass
