2be090195db41b27ee1bc7606c00ac8a9596584b
Ian Duncan <ianduncan08@gmail.com>
2018-07-24 16:02:23 +0300

    [ch #159054006] Use SECRET KEY from .env file

--- authors/settings.py
+++ authors/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7pgozr2jn7zs_o%i8id6=rddie!*0f0qy3$oy$(8231i^4*@u3'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#Check the env.example file to get the idea of how your .env will be structured
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
