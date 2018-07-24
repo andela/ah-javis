1dd30f699c7ad69f1bef318e47337250099faaaa
asamisellah <asami.rachael@gmail.com>
2018-07-24 21:57:13 +0300

    [ch #159053998] Modify test_model inline comments to include '.' at the end

--- authors/apps/authentication/tests/test_urls.py
+++ authors/apps/authentication/tests/test_urls.py
@@ -6,14 +6,16 @@ class TestUrls(TestCase):
     """ Test that URLs exist. """
 
     def test_registration_url(self):
-        """ Tests if url for registration exists """
+        """ Tests if url for registration exists. """
         url = reverse("authentication:registration")
         self.assertEqual(resolve(url).view_name, "authentication:registration")
 
     def test_login_url(self):
+        """ Tests if url for login exists. """
         url = reverse("authentication:login")
         self.assertEqual(resolve(url).view_name, "authentication:login")
 
     def test_user_retreive_update_url(self):
+        """ Tests if url for updating user exists. """
         url = reverse("authentication:update_user")
         self.assertEqual(resolve(url).view_name, "authentication:update_user")
