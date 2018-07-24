6ba88e3d9df20a28fec872bf489bec4007c462bc
asamisellah <asami.rachael@gmail.com>
2018-07-24 15:38:19 +0300

    [ch #159053998] Add unit tests for existing authentication urls

---
+++ authors/apps/authentication/tests/test_urls.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+
+class TestUrls(TestCase):
+    """ Test that URLs exist. """
+
+    def test_registration_url(self):
+        """ Tests if url for registration exists """
+        url = reverse("authentication:registration")
+        self.assertEqual(resolve(url).view_name, "authentication:registration")
+
+    def test_login_url(self):
+        url = reverse("authentication:login")
+        self.assertEqual(resolve(url).view_name, "authentication:login")
+
+    def test_user_retreive_update_url(self):
+        url = reverse("authentication:update_user")
+        self.assertEqual(resolve(url).view_name, "authentication:update_user")
