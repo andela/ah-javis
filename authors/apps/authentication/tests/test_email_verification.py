6a1b81bb8d17a0160dabb9246e021978edeaed49
DennisWanjiru <denniswanjiru71@gmail.com>
2018-08-01 23:28:31 +0300

    [ft #159054009] Test if account has been verified

--- authors/apps/authentication/tests/test_email_verification.py
+++ authors/apps/authentication/tests/test_email_verification.py
@@ -4,6 +4,13 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from ..models import User
 from .utils import TEST_USER
+from django.core import mail
+from ..views import VerifyAccount
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from ..utils import generate_token
+from rest_framework.test import force_authenticate
+from rest_framework.test import APIRequestFactory
 
 
 class TestLogin(APITestCase):
@@ -25,3 +32,19 @@ class TestLogin(APITestCase):
 
         # Verify that the subject of the first message is correct.
         self.assertEqual(mail.outbox[0].subject, 'Verify your account')
+
+    def test_account_has_been_verified(self):
+        """ Tests that the account is verified """
+        self.register_user(TEST_USER)
+        user = User.objects.get()
+        token = generate_token.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk)).decode("utf-8")
+        request = APIRequestFactory().get(
+            reverse("authentication:verify", args=[uid, token]))
+        verify_account = VerifyAccount.as_view()
+        response = verify_account(request, uidb64=uid, token=token)
+        self.assertTrue(response.status_code, 200)
+        user = User.objects.get()
+
+        # Verify that the account has been verified
+        self.assertTrue(user.is_confirmed)
