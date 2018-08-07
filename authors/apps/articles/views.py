3ab1e5fb40a2267f8af89811742867189d777fba
DennisWanjiru <denniswanjiru71@gmail.com>
2018-08-07 12:03:11 +0300

    [ft #159054016] Create views for like and dislike

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from .serializers import ArticleSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Article
 
 # Create your views here.
+
+
+class LikesAPIView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    renderer_classes = (ArticleJSONRenderer, )
+    serializer_class = ArticleSerializer
+
+    def put(self, request, slug):
+        serializer_context = {'request': request}
+
+        try:
+            serializer_instance = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound("An article with this slug does not exist")
+
+        if serializer_instance in Article.objects.filter(dislikes=request.user):
+            serializer_instance.dislikes.remove(request.user)
+
+        serializer_instance.likes.add(request.user)
+
+        serializer = self.serializer_class(
+            serializer_instance, context=serializer_context, partial=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class DislikesAPIView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    renderer_classes = (ArticleJSONRenderer, )
+    serializer_class = ArticleSerializer
+
+    def put(self, request, slug):
+        serializer_context = {'request': request}
+
+        try:
+            serializer_instance = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound("An article with this slug does not exist")
+
+        if serializer_instance in Article.objects.filter(likes=request.user):
+            serializer_instance.likes.remove(request.user)
+
+        serializer_instance.dislikes.add(request.user)
+
+        serializer = self.serializer_class(
+            serializer_instance,  context=serializer_context, partial=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
