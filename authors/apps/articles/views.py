c2c806e692810afc14101643405b94521e7cf9fd
DennisWanjiru <denniswanjiru71@gmail.com>
2018-08-07 16:03:10 +0300

    [ft #159054016] import ArticleJSONRender

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from .models import Article
+from .renders import ArticleJSONRenderer
 
 # Create your views here.
 
