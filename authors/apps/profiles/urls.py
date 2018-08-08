000653479a97ec15efa5bad93946ebcab119520d
asamisellah <asami.rachael@gmail.com>
2018-08-08 05:53:14 +0300

    [ft #159054017] Create view for follow/unfollow feature

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
-from .views import ProfileRetrieveAPIView
+from .views import (ProfileRetrieveAPIView, ProfileFollowAPIView)
 
 app_name = 'profiles'
 
 urlpatterns = [
     url(r'^(?P<username>\w+)/?$',
         ProfileRetrieveAPIView.as_view(), name="view_profile"),
-
+    url(r'^(?P<username>\w+)/follow/?$',
+        ProfileFollowAPIView.as_view(), name="follow_profile"),
 ]
--- authors/apps/profiles/views.py
+++ authors/apps/profiles/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import serializers, status
+from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound
 from .models import Profile
 from .renderers import ProfileJSONRenderer
 from .serializers import ProfileSerializer
@@ -27,9 +29,45 @@ class ProfileRetrieveAPIView(RetrieveAPIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class FollowAPI():
-    pass
+class ProfileFollowAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ProfileJSONRenderer,)
+    serializer_class = ProfileSerializer
 
+    def delete(self, request, username):
+        ''' The current user is able to unfollow another user's profile. '''
+        follower = request.user.profile
 
-class Unfollow():
-    pass
+        # Get the profile of user being followed
+        try:
+            followed = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('The user with this profile does not exist')
+
+        follower.unfollow(followed)
+
+        serializer = self.serializer_class(follower, context={
+            'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, username):
+        ''' The current user is able to follow another user's profile. '''
+        follower = request.user.profile
+
+        # Get the profile of user being followed
+        try:
+            followed = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise NotFound('The user with this profile does not exist')
+
+        # A user cannot follow themselves
+        if follower.pk is followed.pk:
+            raise serializers.ValidationError('You cannot follow yourself')
+
+        follower.follow(followed)
+
+        serializer = self.serializer_class(follower, context={
+            'request': request})
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
