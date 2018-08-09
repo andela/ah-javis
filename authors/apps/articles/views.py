be54b7db706eb01db94d263f3a6f620ab8514582
Ian Duncan <ianduncan08@gmail.com>
2018-08-09 11:09:11 +0300

    [ft #159054018] Create favorite view

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -3,8 +3,9 @@ from rest_framework import status, mixins, viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import NotFound, PermissionDenied
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import ArticleSerializer
-from .renders import ArticleJSONRenderer
+from .renderers import ArticleJSONRenderer, FavoriteJSONRenderer
 from .models import Article
 
 
@@ -38,7 +39,7 @@ class ArticleAPIView(mixins.CreateModelMixin,
         queryset = Article.objects.all()
         serializer = self.serializer_class(
             queryset, many=True)
-     
+
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, slug):
@@ -102,3 +103,63 @@ class ArticleAPIView(mixins.CreateModelMixin,
                 'You do not have permission to delete this article')
 
         return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
+class FavoriteAPIView(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    renderer_classes = (FavoriteJSONRenderer,)
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+    def get(self, request, slug):
+        """
+        Override the retrieve method to get a article
+        """
+        serializer_context = {'request': request}
+        try:
+            serializer_instance = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound("An article with this slug doesn't exist")
+
+        serializer = self.serializer_class(
+            serializer_instance,
+            context=serializer_context
+        )
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, slug):
+        """
+        Method that favorites articles.
+        """
+        serializer_context = {'request': request}
+        try:
+            article = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound("An article with this slug does not exist")
+
+        request.user.profile.favorite(article)
+
+        serializer = self.serializer_class(
+            article,
+            context=serializer_context
+        )
+        return Response(serializer.data,  status=status.HTTP_201_CREATED)
+
+    def delete(self, request, slug):
+        """
+        Method that favorites articles.
+        """
+        serializer_context = {'request': request}
+        try:
+            article = self.queryset.get(slug=slug)
+        except Article.DoesNotExist:
+            raise NotFound("An article with this slug does not exist")
+
+        request.user.profile.unfavorite(article)
+
+        serializer = self.serializer_class(
+            article,
+            context=serializer_context
+        )
+        return Response(serializer.data,  status=status.HTTP_200_OK)
