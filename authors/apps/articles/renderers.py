d9890006f2f41be45ffd68a54e06dcb71f4d8799
Ian Duncan <ianduncan08@gmail.com>
2018-08-09 11:10:19 +0300

    [ft #159054018] Create favorite renderer

---
+++ authors/apps/articles/renderers.py
@@ -0,0 +1,37 @@
+import json
+from rest_framework.renderers import JSONRenderer
+
+
+class ArticleJSONRenderer(JSONRenderer):
+    charset = 'utf-8'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        if data is not None:
+            if len(data) <= 1:
+                return json.dumps({
+                    'article': data
+                })
+            return json.dumps({
+                'articles': data,
+                'articlesCount': len(data)
+            })
+        return json.dumps({
+            "article": 'No article found.'
+        })
+
+
+class FavoriteJSONRenderer(JSONRenderer):
+    charset = 'utf-8'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        if data is not None:
+            if len(data) <= 1:
+                return json.dumps({
+                    'favorite': data
+                })
+            return json.dumps({
+                'favorites': data
+            })
+        return json.dumps({
+            "favorite": 'No favorite article found.'
+        })
