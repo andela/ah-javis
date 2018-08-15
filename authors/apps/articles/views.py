6ecf0b46bd2e9222a07d18787a8052d9e0746116
DennisWanjiru <denniswanjiru71@gmail.com>
2018-08-15 10:50:00 +0300

    [ft #159054020] Add pagination to the articles

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,4 +1,5 @@
 """ Views for django Articles. """
+import os
 from django.shortcuts import render
 from django.db.models import Avg
 
@@ -9,10 +10,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, mixins, viewsets
 from rest_framework.generics import RetrieveAPIView, CreateAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Article, Rate, Comment
 from .serializers import ArticleSerializer, CommentSerializer, RateSerializer
 from .renderers import ArticleJSONRenderer, CommentJSONRenderer, RateJSONRenderer, FavoriteJSONRenderer
+
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = os.getenv('PAGE_SIZE')
+    page_size_query_param = 'page_size'
+    max_page_size = os.getenv('PAGE_SIZE')
 
 
 class LikesAPIView(APIView):
@@ -130,6 +138,7 @@ class ArticleAPIView(mixins.CreateModelMixin,
     queryset = Article.objects.annotate(average_rating=Avg("rate__ratings"))
     permission_classes = (IsAuthenticatedOrReadOnly, )
     serializer_class = ArticleSerializer
+    pagination_class = LargeResultsSetPagination
     renderer_classes = (ArticleJSONRenderer, )
 
     def create(self, request):
@@ -144,19 +153,6 @@ class ArticleAPIView(mixins.CreateModelMixin,
         serializer.save(author=request.user.profile)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def list(self, request):
-        """
-        Get all articles
-        """
-        serializer_context = {'request': request}
-        queryset = Article.objects.annotate(
-            average_rating=Avg("rate__ratings"))
-        serializer = self.serializer_class(
-            queryset, many=True,
-            context=serializer_context)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, slug):
         """
